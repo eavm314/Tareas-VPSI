af6e480a5e60eb3a1073d01d123ee5b2ee5150d8
enrique <enriqueadhemar@gmail.com>
2023-09-29 16:50:28 -0400

    Tarea 1

---
+++ src/test/java/example3/InvertirNumTest.java
@@ -0,0 +1,28 @@
+package example3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class InvertirNumTest {
+    @ParameterizedTest
+    @CsvSource(
+            {
+                "0,0",
+                "2,2",
+                "21,12",
+                "123,321",
+                "2147483647,7463847412",
+                "-1,Valor Incorrecto",
+                "-45, Valor Incorrecto",
+                "-2147483648, Valor Incorrecto",
+            }
+    )
+    public void verifyTest(int num, String invertedNum){
+        InvNumber inv = new InvNumber();
+
+        String actualResult = inv.invertir(num);
+
+        Assertions.assertEquals(invertedNum, actualResult);
+    }
+}
